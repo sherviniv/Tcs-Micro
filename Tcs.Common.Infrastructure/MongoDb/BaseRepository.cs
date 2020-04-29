fa3076d5359a4e8248f096b0ffa3afbc2139327b
sherviniv@gmail.com <sherviniv@gmail.com>
2020-04-29 20:42:40 +0430

    common infra add mongooptions

---
+++ Tcs.Common.Infrastructure/MongoDb/BaseRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Tcs.Common.Domain.Exceptions;
+
+namespace Tcs.Common.Infrastructure.MongoDb
+{
+    public class BaseRepository
+    {
+        protected readonly IMongoDatabase MongoDatabase;
+
+        protected BaseRepository(MongoDbSettings settings)
+        {
+            if(settings == null) 
+                throw new TcsException("empty_settings",
+                     "Database settings must be defined.");
+
+            if (string.IsNullOrEmpty(settings.ServerConnection))
+                throw new TcsException("empty_settings_ServerConnection",
+                     "ServerConnection can not be empty.");
+
+            if (string.IsNullOrEmpty(settings.Database))
+                throw new TcsException("empty_settings_Database",
+                     "Database name must be defined.");
+
+            var client = new MongoClient(settings.ServerConnection);
+
+            MongoDatabase = client.GetDatabase(settings.Database,
+                new MongoDatabaseSettings
+                {
+                    GuidRepresentation = GuidRepresentation.Standard
+                });
+        }
+    }
+}
